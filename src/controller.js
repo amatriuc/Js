7af288d991d4c89f97b9b699ad7ea98a59317abe
amatriuc <alexandr.matriuc@gmail.com>
2017-11-26 22:46:10 +0200

    Js learning. 01:00:00 https://www.youtube.com/watch?v=p3CXpKIisPA&t=6763s

---
+++ src/controller.js
@@ -0,0 +1,16 @@
+class Controller {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+
+        view.on('add', this.addTodo.bind(this));
+    }
+
+    addTodo(title) {
+        this.model.addItem({
+            id: Date.now(),
+            title,
+            completed: false
+        })
+    }
+}
